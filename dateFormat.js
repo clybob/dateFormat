ebf9acf5994b1e5589fadbcbf574bc7424ad55d2
Romulo Tavares <romulo.tavares@corp.globo.com>
2012-06-03 16:19:31 -0300

    Created default return of format method

--- dateFormat.js
+++ dateFormat.js
@@ -1,3 +1,36 @@
 Date.prototype.format = function(){
-	return true;
-}
+	var formatMonth = function(month, numberOfDigits){
+		month += 1;
+		month = insertZerosLeft(month, numberOfDigits);
+		return month;
+	};
+
+	var insertZerosLeft = function(data, numberOfDigits){
+		data = data.toString();
+		var zeros = '';
+		var qtdZerosLeft = numberOfDigits - data.length;
+
+		for(var count = 0; count < qtdZerosLeft; count++){
+			zeros += '0';
+		}
+
+		data = zeros + data;
+		return data;
+	};
+
+	var dateFormated = [];
+	dateFormated.push( this.getFullYear() );
+	dateFormated.push( formatMonth(this.getMonth(), 2) );
+	dateFormated.push( insertZerosLeft(this.getDate(), 2) );
+
+	var timeFormated = [];
+	timeFormated.push( insertZerosLeft(this.getHours(), 2) );
+	timeFormated.push( insertZerosLeft(this.getMinutes(), 2) );
+	timeFormated.push( insertZerosLeft(this.getSeconds(), 2) );
+
+	var datetimeFomated = dateFormated.join('-');
+	datetimeFomated += ' ';
+	datetimeFomated += timeFormated.join(':');
+
+	return datetimeFomated;
+};
--- tests/test_dateFormat.js
+++ tests/test_dateFormat.js
@@ -1,3 +1,8 @@
 test( "test date prototype should have format method", function() {
-  ok( Date.prototype.format );
+	ok( Date.prototype.format );
+});
+
+test( "test call method should display YYYY-MM-DD HH-MM-SS by default", function() {
+	var date = new Date("2012-06-02 13:10:00");
+	ok( date.format() === "2012-06-02 13:10:00" );
 });
